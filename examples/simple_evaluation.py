a4e7cd419031fda08c6e89aa5ca5707e3c1bea4e
Joan Alabort <joan.alabort@hudl.com>
2016-07-26 22:01:32 +0100

    Add notebooks, scripts and data folders

---
+++ examples/simple_evaluation.py
@@ -0,0 +1,50 @@
+import json
+from pymot import MotEvaluation, check_format
+
+
+annotations_file = '../data/annotations.json'
+hypotheses_file = '../data/hypotheses.json'
+
+
+
+a = open(annotations_file)
+
+if annotations_file.endswith('.json'):
+    annotations = json.load(a)[0]
+else:
+    raise Exception('Only json is supported!')
+
+a.close()
+
+
+
+h = open(hypotheses_file)
+
+if hypotheses_file.endswith('.json'):
+    hypotheses = json.load(h)[0]
+else:
+    raise Exception('Only json is supported!')
+
+h.close()
+
+
+
+a_correct, h_correct = check_format(annotations, hypotheses)
+if not a_correct :
+    raise ValueError('Format of file %s is incorrect!') % annotations_file
+elif not h_correct :
+    raise ValueError('Format of file %s is incorrect!') % hypotheses_file
+
+
+
+evaluator = MotEvaluation(annotations, hypotheses)
+evaluator.evaluate()
+
+
+print('Track statistics')
+evaluator.print_track_statistics()
+
+print('')
+
+print('Results')
+evaluator.print_results()
